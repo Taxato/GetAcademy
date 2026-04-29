5496f9e2f9e9a1358589119cd44e45e86288e628
Taxato <taxatoftw@gmail.com>
2026-04-29 13:53:13 +0200

    Finished emne 2 oblig ?

--- Emne 2/ObligVår2026/controller.js
+++ Emne 2/ObligVår2026/controller.js
@@ -15,11 +15,82 @@ function handleSearch(searchTerm) {
 	updateView();
 }
 
-function filterErrors() {
+function filterErrorsBySearch() {
 	const searchTerm = model.inputs.search.searchTerm.toLowerCase();
 	return model.data.errors.filter(
 		error =>
 			error.title.toLowerCase().includes(searchTerm) ||
 			error.description.toLowerCase().includes(searchTerm)
 	);
+}
+
+function filterErrorsByStatus() {
+	return model.data.errors.filter(
+		error =>
+			model.inputs.overview.activeFilter === "all" ||
+			error.status === model.inputs.overview.activeFilter
+	);
+}
+
+function sortErrorsByPriority(errors) {
+	const priorityToNumber = pri => ["low", "medium", "high"].indexOf(pri);
+	return [...errors].sort((a, b) => {
+		const aPri = priorityToNumber(a.priority);
+		const bPri = priorityToNumber(b.priority);
+		return model.inputs.overview.prioritySortDirection === "ascending"
+			? aPri - bPri
+			: bPri - aPri;
+	});
+}
+
+function handleTogglePrioritySort() {
+	model.inputs.overview.prioritySortDirection =
+		model.inputs.overview.prioritySortDirection === "ascending"
+			? "descending"
+			: "ascending";
+	updateView();
+}
+
+function handleAddError() {
+	const modelData = model.inputs.addError;
+	model.data.latestErrorId += 1;
+	const newError = {
+		id: model.data.latestErrorId,
+		title: modelData.title,
+		description: modelData.description,
+		severity: modelData.severity,
+		priority: modelData.priority,
+		status: "open",
+		personId: Number(modelData.personId) || null,
+	};
+	model.data.errors.push(newError);
+
+	// Clear inputs in model
+	model.inputs.addError = {
+		title: "",
+		description: "",
+		severity: "",
+		priority: "",
+		personId: null,
+	};
+
+	selectPage("overview");
+}
+
+function handleDeleteError(errorId) {
+	const error = model.data.errors.find(err => err.id === errorId);
+
+	// Don't delete if error bug is status "open"
+	if (error.status === "open") return;
+
+	model.data.errors.splice(model.data.errors.indexOf(error), 1);
+
+	updateView();
+}
+
+function handleToggleErrorStatus(errorId) {
+	const error = model.data.errors.find(err => err.id === errorId);
+	error.status = error.status === "open" ? "closed" : "open";
+
+	updateView();
 }
--- Emne 2/ObligVår2026/view.js
+++ Emne 2/ObligVår2026/view.js
@@ -4,7 +4,7 @@ function updateView() {
 	const cursor = searchInput?.selectionStart;
 	const hadFocus = document.activeElement === searchInput;
 
-	document.getElementById("app").innerHTML = /*HTML*/ `
+	document.getElementById("app").innerHTML = /*html*/ `
         <h1>BugTracker</h1>
         ${createButtonsHtml()}
         ${renderPage()}
@@ -19,14 +19,22 @@ function updateView() {
 }
 
 function overviewHtml() {
-	return /*HTML*/ `
+	return /*html*/ `
 		<h2>Oversikt</h2>
 		<div class="filter-group">
-			<button onclick="handleSelectFilter('all')" ${isButtonActive("all")}>Alle</button>
-			<button onclick="handleSelectFilter('open')" ${isButtonActive("open")}>Open</button>
-			<button onclick="handleSelectFilter('closed')" ${isButtonActive("closed")}>Closed</button>
+			<div class="status-filter-group">
+				<button onclick="handleSelectFilter('all')" ${isButtonActive("all")}>Alle</button>
+				<button onclick="handleSelectFilter('open')" ${isButtonActive("open")}>Open</button>
+				<button onclick="handleSelectFilter('closed')" ${isButtonActive("closed")}>Closed</button>
+			</div>
+			<button 
+				class="priority-sort-button sort-direction-${model.inputs.overview.prioritySortDirection}"
+				onclick="handleTogglePrioritySort()">
+				<span>Prioritet</span>
+				<img src="./public/chevron-right.svg" />
+			</button>
 		</div>
-		${errorsHtml(model.data.errors)}
+		${errorsHtml(sortErrorsByPriority(filterErrorsByStatus()))}
 	`;
 }
 
@@ -34,7 +42,7 @@ function errorsHtml(errors) {
 	return errors
 		.map(error => {
 			const person = model.data.persons.find(p => p.id == error.personId);
-			return /*HTML*/ `
+			return /*html*/ `
 			<div class="error-card">
 				<h3>${error.title}</h3>
 				<p>Description: ${error.description}</p>
@@ -43,10 +51,11 @@ function errorsHtml(errors) {
 					<b>${person ? person.name : "Ikke satt"}</b>
 				</p>
 				<p class="severity-${error.severity}">Severity: ${error.severity}</p>
+				<p class="priority-${error.priority}">Priority: ${error.priority}</p>
 				<p>Status: ${error.status}</p>
-				<button onclick="deleteError()">Slett</button>
-				<button onclick="toggleStatus()">
-					${error.status === "open" ? "Merk som closed" : "Merk som open"}
+				<button onclick="handleDeleteError(${error.id})">Slett</button>
+				<button onclick="handleToggleErrorStatus(${error.id})">
+					${error.status === "open" ? "Marker som closed" : "Marker som open"}
 				</button>
 			</div>
 		`;
@@ -55,11 +64,11 @@ function errorsHtml(errors) {
 }
 
 function createButtonsHtml() {
-	return /*HTML*/ `
+	return /*html*/ `
 	<div class="nav-buttons">
 		<button ${isActive("overview")} onclick="selectPage('overview')">Oversikt</button>
 		<button ${isActive("search")} onclick="selectPage('search')">Søk</button>
-		<button ${isActive("addErrors")} onclick="selectPage('addErrors')">Legg til</button>
+		<button ${isActive("addError")} onclick="selectPage('addError')">Legg til</button>
 	</div>
 	`;
 }
@@ -77,13 +86,13 @@ function renderPage() {
 		return overviewHtml();
 	} else if (model.app.page == "search") {
 		return searchErrorsHtml();
-	} else if (model.app.page == "addErrors") {
-		return addErrorsHtml();
+	} else if (model.app.page == "addError") {
+		return addErrorHtml();
 	}
 }
 
 function searchErrorsHtml() {
-	let html = /*HTML*/ `
+	let html = /*html*/ `
 		<h2>Søk</h2>
 		<input
 			id="search-input"
@@ -92,42 +101,68 @@ function searchErrorsHtml() {
 			type="text" 
 			placeholder="Søk etter feil..."/>
 
-		${errorsHtml(filterErrors())}
+		${errorsHtml(filterErrorsBySearch())}
 	`;
 
 	return html;
 }
 
-function addErrorsHtml() {
-	return /*HTML*/ `
+function addErrorHtml() {
+	return /*html*/ `
 		<h2>Legg til en feil</h2>
-		<div class="add-error-form">
+		<form onsubmit="event.preventDefault(); handleAddError()" class="add-error-form">
 			<input
+				name="title"
+				value="${model.inputs.addError.title}"
+				onchange="model.inputs.addError.title = this.value"
+				required
 				type="text"
 				placeholder="Tittel" />
 			<textarea
+				name="description"
+				value="${model.inputs.addError.description}"
+				onchange="model.inputs.addError.description = this.value"
+				required
 				style="resize: none"
 				placeholder="Beskriv feilen"></textarea>
 			<label>Alvorlighetsgrad:</label>
-			<select>
+			<select 
+				name="severity"
+				required
+				onchange="model.inputs.addError.severity = this.value">
 				<option value="">-- Velg --</option>
-				<option value="low">Low</option>
-				<option value="medium">Medium</option>
-				<option value="high">High</option>
+				<option value="low" ${model.inputs.addError.severity === "low" ? "selected" : ""}>Low</option>
+				<option value="medium" ${model.inputs.addError.severity === "medium" ? "selected" : ""}>Medium</option>
+				<option value="high" ${model.inputs.addError.severity === "high" ? "selected" : ""}>High</option>
+			</select>
+			<label>Prioritet:</label>
+			<select 
+				name="priority"
+				required
+				onchange="model.inputs.addError.priority = this.value">
+				<option value="">-- Velg --</option>
+				<option value="low" ${model.inputs.addError.priority === "low" ? "selected" : ""}>Low</option>
+				<option value="medium" ${model.inputs.addError.priority === "medium" ? "selected" : ""}>Medium</option>
+				<option value="high" ${model.inputs.addError.priority === "high" ? "selected" : ""}>High</option>
 			</select>
 			<label>Ansvarlig:</label>
-			<select>
+			<select 
+				name="personId"
+				onchange="model.inputs.addError.personId = this.value">
 				<option value="">-- Ikke satt --</option>
 				${model.data.persons
 					.map(
-						p => `
-				<option value="${p.id}">${p.name}</option>
-				`
+						p => /*html*/ `
+							<option 
+								value="${p.id}" 
+								${model.inputs.addError.personId == p.id ? "selected" : ""}>
+								${p.name}
+							</option>`
 					)
 					.join("")}
 			</select>
 			<button>Lagre</button>
-		</div>
+		</form>
 	`;
 }
 
